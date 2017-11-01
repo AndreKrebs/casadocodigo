e5860d608afcc7de145a6c8ed702226e90bc1f8c
André Krebs <andre.krebssilva@gmail.com>
2017-10-31 23:29:03 -0200

    Criada a classe de precos, enum de tipoPreco e lista de preco com notation ElementCollection para relacionar tipo de preco com produto. Ajustes no front e no controller para listar os tipos de preço

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,9 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +20,9 @@ public class Produto {
 	private String titulo;
 	private String descricao;
 	private int paginas;
+	@ElementCollection
+	@CollectionTable(name="produto_precos")
+	private List<Preco> precos;
 	
 	
 	public int getId() {
@@ -42,7 +49,12 @@ public class Produto {
 	public void setPaginas(int paginas) {
 		this.paginas = paginas;
 	}
-	
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
 	
 	@Override
 	public String toString() {
