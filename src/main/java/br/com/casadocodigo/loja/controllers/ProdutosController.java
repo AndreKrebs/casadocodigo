d4f817c587dd2371bdb1ebec5c0576c9a339c235
André Krebs <andre.krebssilva@gmail.com>
2017-11-16 22:35:01 -0200

    Validação simples do formulário

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,7 +1,12 @@
 package br.com.casadocodigo.loja.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("/produtos")
@@ -18,6 +24,10 @@ public class ProdutosController {
 	@Autowired
 	private ProdutoDAO produtoDAO;
 	
+	@InitBinder
+	public void InitBinder(WebDataBinder binder){
+	    binder.addValidators(new ProdutoValidation());
+	}
 	
 	@RequestMapping("/form")
 	public ModelAndView form() {
@@ -28,8 +38,11 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes) {
-		System.out.println(produto.toString());
+	public ModelAndView gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
+		
+		if(result.hasErrors()){
+	        return form();
+	    }
 		
 		produtoDAO.gravar(produto);
 		
